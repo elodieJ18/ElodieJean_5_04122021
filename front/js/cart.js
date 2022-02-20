1fc118e21a2ccfd93664c808befc38e6dd7e431c
Elodie Jean <elodie.jean18@gmail.com>
2022-02-20 23:10:41 +0100

    cart

---
+++ front/js/cart.js
@@ -0,0 +1,40 @@
+let addProduct = JSON.parse(localStorage.getItem("product"));
+
+console.log(addProduct);
+
+const cartDisplay = async () => {
+  console.log("salut");
+
+  if (addProduct) {
+    await addProduct;
+    console.log(addProduct);
+
+    document.getElementById("cart__items").innerHTML = addProduct.map(
+      (
+        product
+      ) => `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+      <div class="cart__item__img">
+        <img src="${product.imageUrl}" alt="${product.altTxt}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${product.name}</h2>
+          <p>${product.colors}</p>
+          <p>${product.price}€</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+          </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+          </div>
+        </div>
+      </div>
+    </article>`
+    );
+  }
+};
+
+cartDisplay();
